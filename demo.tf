27094b447801de49c0e782a156add7dcd2929df2
Amlan Das <dasamlan271988@gmail.com>
2023-12-21 00:20:55 +0530

    terraform commit

--- demo.tf
+++ demo.tf
@@ -2,6 +2,21 @@ variable "sample" {
      default = "batch56"
 }
 
-output "demo_check" {
-    value = "The batch at present is ${var.sample}"
+# output "demo_check" {
+#     value = "The batch at present is ${var.sample}"
+# }
+
+variable "sample1" {
+    default = 100
+}
+
+variable "sample2" {
+    default = true
+}
+
+variable "sample3" {
+    default = "Amlan"
+}
+output "print" {
+    value = "This is ${var.sample3} from ${var.sample} who scored ${var.sample1} marks in ${var.sample2} exams "
 }
