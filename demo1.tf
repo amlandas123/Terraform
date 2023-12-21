84964fa5842c2d83985eaa2186ac866476364949
Amlan Das <dasamlan271988@gmail.com>
2023-12-21 15:23:00 +0530

    terraform commit

--- demo1.tf
+++ demo1.tf
@@ -4,3 +4,9 @@ variable "country" {}
 output "name_country" {
     value = "The name of the country is ${var.country}"
 }
+
+
+variable "environment" {}
+output "name_env" {
+    value = "name of the environment is ${var.environment}"
+}
