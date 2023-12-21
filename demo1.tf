88eefa1b13f5b07fe41e161026aed07d58616586
Amlan Das <dasamlan271988@gmail.com>
2023-12-21 21:13:35 +0530

    terraform commit

--- demo1.tf
+++ demo1.tf
@@ -1,3 +1,5 @@
+
+#Variable listing
 variable "listing" {
     default = [
         "Amlan",
@@ -13,4 +15,18 @@ output "listing_op" {
     value = "Hi this is ${var.listing[1]} wife of ${var.listing[0]} who was born in the year ${var.listing[3]}"
 }
 
+#variable mapping. Mapping is mostly used instead of listing
+variable "mapping" {
+    default = {
+        name    = "Amlan Das"
+        batch   = 56
+        food    = true
+        city    = "Bangalore"
+        country =  "India"
+    }
+  
+}
 
+output "mapping_op" {
+    value = "This is ${var.mapping{"country"}} and my name is ${var.mapping{"name"}} from ${var.mapping{"city"}} and my batch is ${var.mapping{batch}}"
+}
