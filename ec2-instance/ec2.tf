5ef9f140a3ebd66dc9099e267a2daefb56144021
Amlan Das <dasamlan271988@gmail.com>
2023-12-22 20:40:03 +0530

    From amlan

--- ec2-instance/ec2.tf
+++ ec2-instance/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "app1" {
-  ami           = data.aws_ami.ami.id   # AMI:This are Arguments which we get before machine starts
+  ami           = data.aws_ami.ami1.id   # AMI:This are Arguments which we get before machine starts
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.dasa2024.id] 
   tags = {
