5ae4f5608b1d84ddebed8522b3f202891dc25179
Amlan Das <dasamlan271988@gmail.com>
2023-12-22 14:43:12 +0530

    terraform commit

--- ec2-instance/ec2.tf
+++ ec2-instance/ec2.tf
@@ -1,8 +1,12 @@
 resource "aws_instance" "app1" {
-  ami           = "ami-0f75a13ad2e340a58"
+  ami           = "ami-0f75a13ad2e340a58"     # AMI:This are Arguments which we get pre
   instance_type = "t3.micro"
 
   tags = {
     Name = "Devops-Instance"
   }
 }  
+
+output "instance_arn" {                #IP Address are Attributes which gives post output
+    value = aws_instance.arn
+}
