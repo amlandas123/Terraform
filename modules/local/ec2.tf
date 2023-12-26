3bcee248940a7c71a948fb40942304c73c695c05
Amlan Das <dasamlan271988@gmail.com>
2023-12-26 20:45:51 +0530

    From amlan

--- modules/local/ec2.tf
+++ modules/local/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "app1" {
-  ami           = data.aws_ami.ami1.id   # AMI:This are Arguments which we get before machine starts
+  ami           = "ami-06192c6f2b41b7741"   # AMI:This are Arguments which we get before machine starts
   instance_type = "t2.micro"
 
 }  
