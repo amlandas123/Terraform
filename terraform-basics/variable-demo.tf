1a4f4bbdf3257904f47e56fc90d33d107a8d2d84
Amlan Das <dasamlan271988@gmail.com>
2023-12-31 15:31:10 +0530

    From amlan

--- terraform-basics/variable-demo.tf
+++ terraform-basics/variable-demo.tf
@@ -2,54 +2,62 @@
 # #Variable can be declared in the form of list
 
 # #Variable Listing example
-# variable "listing" {
-#     default = [
-#         "Amlan",
-#         "Priya",
-#         "Mou",
-#         1988,
-#         true
-#     ]
+variable "listing" {
+    default = [
+        "Amlan",
+        "Priya",
+        "Mou",
+        1988,
+        true
+    ]
   
-# }
+}
 
-# output "listing_op" {
-#     value = "Hi this is ${var.listing[1]} wife of ${var.listing[0]} who was born in the year ${var.listing[3]}"
-# }
+output "listing_op" {
+    value = "Hi this is ${var.listing[1]} wife of ${var.listing[0]} who was born in the year ${var.listing[3]}"
+}
 
-# #variable mapping. Mapping is mostly used instead of listing
-# variable "mapping" {
-#     default = {
-#         name    = "Amlan Das"
-#         batch   = 56
-#         food    = true
-#         city    = "Bangalore"
-#         country =  "India"
-#     }
+#variable mapping. Mapping is mostly used instead of listing
+variable "mapping" {
+    default = {
+        name    = "Amlan Das"
+        batch   = 56
+        food    = true
+        city    = "Bangalore"
+        country =  "India"
+    }
   
-# }
+}
 
-# output "mapping_op" {
-#     value = "This is ${var.mapping["country"]} and my name is ${var.mapping["name"]} from ${var.mapping["city"]} and my batch is ${var.mapping["batch"]}"
-# }
+output "mapping_op" {
+    value = "This is ${var.mapping["country"]} and my name is ${var.mapping["name"]} from ${var.mapping["city"]} and my batch is ${var.mapping["batch"]}"
+}
 
-# variable "city" {}
-# output "city_op" {
-#     value = "I live in ${var.city}"
-# }
+variable "city" {}
+output "city_op" {
+    value = "I live in ${var.city}"
+}
 
-# variable "state" {}
-# output "state_op" {
-#     value = "I live in ${var.state}"
-# }
+variable "state" {}
+output "state_op" {
+    value = "I live in ${var.state}"
+}
 
-# variable "country" {}
-# output "country_op" {
-#     value = "Iam from ${var.country}"
-# }
+variable "country" {}
+output "country_op" {
+    value = "Iam from ${var.country}"
+}
 
 variable "mother" {}
 
 output "name" {
   value = "My mother name is ${var.mother}"
 }
+
+variable "father" {}
+variable "office" {}
+output "father_op" {
+  value = "My father ${var.father} is a state govt. employee and I work in ${var.office}"
+}
+
+
