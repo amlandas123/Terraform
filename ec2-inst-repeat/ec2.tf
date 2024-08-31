a810fcbac9cd318f01eea076468b6fb533f276ee
Amlan Das <dasamlan271988@gmail.com>
2024-08-31 12:38:05 +0530

    vol

--- ec2-inst-repeat/ec2.tf
+++ ec2-inst-repeat/ec2.tf
@@ -6,6 +6,17 @@ resource "aws_instance" "app" {
   }
 }
 
+resource "aws_ebs_volume" "devops-volume" {
+  availability_zone = "us-east-1a"
+  size              = 50
+}
+
+resource "aws_volume_attachment" "devops-volume1" {
+  device_name = "/dev/sdb"
+  volume_id   = aws_ebs_volume.devops-volume.id
+  instance_id = aws_instance.app.id
+}
+
 output "instance_dns" {
     value = aws_instance.app.private_dns
   
