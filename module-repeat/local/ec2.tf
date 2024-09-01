bfedb89adf32fd4cabb7791ed999ac556e366fe8
Amlan Das <dasamlan271988@gmail.com>
2024-09-01 15:03:51 +0530

    comment

--- module-repeat/local/ec2.tf
+++ module-repeat/local/ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "app-amlan" {
-  ami = "ami-08a805de956384d1b"
-  instance_type = "t2.micro"
+    ami = "ami-08a805de956384d1b"
+    instance_type = "t2.micro"
 }
 
 output "public_ip" {
   value = aws_instance.app-amlan.public_ip
 }
 
-output "vpc_id" {
-    value = aws_instance.app-amlan.vpc_id
-}
+# output "vpc_id" {
+#     value = aws_vpc.peacock-vpc.id
+# }
--- module-repeat/local/vpc.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_vpc" "peacock-vpc" {
-    cidr_block = "192.168.0.0/24"
-    tags = {
-      Name = "dasa2024-vpc"
-    }
-}
