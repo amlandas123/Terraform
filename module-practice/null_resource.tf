c2b2d8e6204d653fe8d5c68ce5cd5f2aab57545b
Amlan Das <dasamlan271988@gmail.com>
2024-09-02 19:22:44 +0530

    commit1

---
+++ module-practice/null_resource.tf
@@ -0,0 +1,19 @@
+
+resource "null_resource" "playbook" {
+
+    depends_on = [aws_instance.app-amlan]
+
+    connection {
+        type = "ssh"
+        user = "centos"
+        password = "DevOps321"
+        host = aws_instance.app-amlan.private_ip
+  }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+        "mkdir /tmp/test"
+    ]
+  }
+}
